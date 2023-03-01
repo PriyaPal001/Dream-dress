c70b16fc7868d583cca044236420850c5bc3c096
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-03-01 16:30:34 +0530

    added website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<svelte:head>
+    <title>Dream Dress</title>
+</svelte:head>
+
+<div class="carousel rounded-box">
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/free-photo/young-beautiful-stylish-woman-wedding-dress_285396-7822.jpg?w=826&t=st=1677540031~exp=1677540631~hmac=6a2361f0648494b80a130461b397d2df05561817ff6d745a6c0b65904be69149" alt="Burger" class="w-full h-[500px]" />
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/free-photo/beautiful-model-girl-white-dress-man-suit_1328-1779.jpg?w=826&t=st=1677540225~exp=1677540825~hmac=918ca7b63749fd56b72a275a568892aaa0da7a611e612f2edd9a34793c5e51b4" alt="Burger"class="w-full h-[500px]" />
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/premium-photo/beautiful-young-brunette-woman-choosing-wedding-dress-bridal-salon_633611-4120.jpg?w=826" alt="Burger" class="w-full h-[500px]"/>
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/free-photo/elegant-bride-puts-wedding-gown-her-room_146671-19765.jpg?w=826&t=st=1677540176~exp=1677540776~hmac=60262550c6d59c7039d271ca6c53ca04bbe76f2556f1bfb522d6e9799901b5e1" alt="Burger" class="w-full h-[500px]"/>
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/free-photo/young-woman-celebrating-her-quinceanera_23-2149449182.jpg?w=826&t=st=1677541511~exp=1677542111~hmac=b9cf8f633aaaf46c3a0d76d5a0804fe1cc1200bfa98f2a2746ba21b61179e6b1" alt="Burger" class="w-full h-[500px]"/>
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="https://img.freepik.com/free-photo/young-beautiful-stylish-woman-wedding-dress_285396-7876.jpg?w=826&t=st=1677540075~exp=1677540675~hmac=61bfe705f701e43138aae1e0a00eb3db24ba76da2d693de909b350bbda60beff" alt="Burger" class="w-full h-[500px]"/>
+    </div> 
+    <div class="carousel-item w-full">
+      <img src="/https://img.freepik.com/free-photo/beautiful-bride-portrait-beach_23-2149044012.jpg?w=826&t=st=1677541563~exp=1677542163~hmac=2175416175fe5c25b48f4fa669af29d84be8dcdf22257c031f4deb99b9f1b6fc" alt="Burger" class="w-full h-[500px]"/>
+    </div>
+  </div>
+
+  <div class="bg-white py-6 sm:py-8 lg:py-12">
+    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-8 md:mb-12">Collections</h2>
+  
+      <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
+        <!-- product - start -->
+        <div>
+          <a href="/saree" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
+            <img src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+  
+            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
+              <span class="text-gray-500">Men</span>
+              <span class="text-gray-800 text-lg lg:text-xl font-bold">Saree</span>
+            </div>
+          </a>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="/Lahenga" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
+            <img src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by engin akyurt" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+  
+            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
+              <span class="text-gray-500">Women</span>
+              <span class="text-gray-800 text-lg lg:text-xl font-bold">Lahenga</span>
+            </div>
+          </a>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="/Gowns" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
+            <img src="https://images.unsplash.com/photo-1552668693-d0738e00eca8?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+  
+            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
+              <span class="text-gray-500">Men</span>
+              <span class="text-gray-800 text-lg lg:text-xl font-bold">gowns</span>
+            </div>
+          </a>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="/Salwar" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
+            <img src="https://images.unsplash.com/photo-1560269999-cef6ebd23ad3?auto=format&q=75&fit=crop&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+  
+            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
+              <span class="text-gray-500">Women</span>
+              <span class="text-gray-800 text-lg lg:text-xl font-bold">Salwar</span>
+            </div>
+          </a>
+        </div>
+        <!-- product - end -->
+      </div>
+    </div>
+  </div>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100) !important;
+    }
+  </style>
---
+++ src/routes/Lahenga/+page.svelte
@@ -0,0 +1,146 @@
+<h1>Lahenga</h1>
+
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <div class="flex justify-between items-end gap-4 mb-6">
+        <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">Selected</h2>
+  
+        <a href="#" class="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3">Show more</a>
+      </div>
+  
+      <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8">
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Rachit Tank" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+  
+            <span class="bg-red-500 text-white text-sm tracking-wider uppercase rounded-br-lg absolute left-0 top-0 px-3 py-1.5">sale</span>
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Timely Watch</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$15.00</span>
+              <span class="text-red-500 line-through mb-0.5">$30.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Galina N" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Fancy Plant</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$9.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by eniko kis" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Elderly Cam</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$45.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Irene Kredenets" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Shiny Shoe</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$29.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1528476513691-07e6f563d97f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Charles Deluvio" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Spiky Plant</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$4.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1612033448550-9d6f9c17f07d?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Fernando Lavin" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Wieldy Film</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$19.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1579609598065-79f8e5bcfb70?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Kiran CK" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+            <span class="bg-red-500 text-white text-sm tracking-wider uppercase rounded-br-lg absolute left-0 top-0 px-3 py-1.5">sale</span>
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Sturdy Stand</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$12.00</span>
+              <span class="text-red-500 line-through mb-0.5">$24.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+  
+        <!-- product - start -->
+        <div>
+          <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
+            <img src="https://images.unsplash.com/photo-1619066045029-5c7e8537bd8c?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Fakurian Design" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+          </a>
+  
+          <div>
+            <a href="#" class="text-gray-500 hover:gray-800 lg:text-lg transition duration-100 mb-1">Lazy Bottle</a>
+  
+            <div class="flex items-end gap-2">
+              <span class="text-gray-800 lg:text-lg font-bold">$9.00</span>
+            </div>
+          </div>
+        </div>
+        <!-- product - end -->
+      </div>
+    </div>
+  </div>
