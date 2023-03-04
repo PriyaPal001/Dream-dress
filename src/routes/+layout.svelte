4d094ebb8c7981ee49eb9d7b85264af0b80c5bdc
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-03-04 22:09:40 +0530

    Added Suttf

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
-    import "../app.css";
+  import "../app.css";
+  import Dress from "$lib/dress.png";
 </script>
 
-<div class="navbar bg-base-100" id="firstnav">
+<!-- Changes : bg-base-100 to bg-gray-900 -->
+<div class="navbar bg-gray-900" id="firstnav">
   <div class="flex-1">
     <!-- svelte-ignore a11y-missing-attribute -->
-    <a class="btn btn-ghost normal-case text-xl " href="/">Dream Dress</a>
+    <a class="btn btn-ghost normal-case text-xl text-sky-400 " href="/">
+      <img src={Dress} width="40" alt="Logo" class="rounded-full mx-2" />
+      Dream Dress</a
+    >
   </div>
   <div class="flex-none gap-2">
     <!-- <div class="form-control">
@@ -13,14 +18,21 @@
     </div> -->
     <div class="dropdown dropdown-end">
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <!-- svelte-ignore a11y-label-has-associated-control -->
       <label tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-8 rounded-full">
           <!-- svelte-ignore a11y-missing-attribute -->
-          <img src="https://img.freepik.com/free-vector/bride-silhouette-with-white-dress_23-2147494158.jpg?w=740&t=st=1677539114~exp=1677539714~hmac=536491e5eb7f46ba060a3db9581a0b580a130cfdf5176cc0bad47386bdfd8d22" />
+          <img
+            src="https://img.freepik.com/free-vector/bride-silhouette-with-white-dress_23-2147494158.jpg?w=740&t=st=1677539114~exp=1677539714~hmac=536491e5eb7f46ba060a3db9581a0b580a130cfdf5176cc0bad47386bdfd8d22"
+          />
           <!-- https://img.freepik.com/premium-vector/white-bride-dress_23-2147493293.jpg?w=740 -->
         </div>
       </label>
-      <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <ul
+        tabindex="0"
+        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+      >
         <li>
           <!-- svelte-ignore a11y-missing-attribute -->
           <a class="justify-between">
@@ -28,79 +40,153 @@
             <span class="badge">New</span>
           </a>
         </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a href="/">Settings</a></li>
+        <li><a href="/">Logout</a></li>
       </ul>
     </div>
   </div>
 </div>
-<hr>
-<div class="navbar bg-base-100 subnav " id="secnav">
-    <ul class="mx-2 text-justify justify-between space-x-4" >
-        <li class=""><a href="/saree">Saree</a></li>
-        <li class=""><a href="/Gowns">Gowns</a></li>
-        <li class=""><a href="/Lahenga">Lahenga</a></li>
-        <li class=""><a href="/Salwar">Salwar</a></li>
-    </ul>
-  </div>
+<hr />
+<div
+  class="navbar text-center flex justify-center bg-gray-900 md:justify-start   md:bg-gray-800"
+  id="secnav"
+>
+  <ul class="mx-2 flex justify-center align-middle text-center  space-x-4">
+    <li class="hover:text-cyan-400 transition duration-150">
+      <a href="/saree">Saree</a>
+    </li>
+    <li class="hover:text-cyan-400 transition duration-150">
+      <a href="/Gowns">Gowns</a>
+    </li>
+    <li class="hover:text-cyan-400 transition duration-150">
+      <a href="/Lahenga">Lahenga</a>
+    </li>
+    <li class="hover:text-cyan-400 transition duration-150">
+      <a href="/Salwar">Salwar</a>
+    </li>
+  </ul>
+</div>
 
-<slot/>
+<slot />
 <footer class="text-gray-600 body-font">
-  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+  <div
+    class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col"
+  >
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a
+      class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
       </svg>
       <span class="ml-3 text-xl">Tailblocks</span>
     </a>
-    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
-      <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+    <p
+      class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
+    >
+      © 2020 Tailblocks —
+      <a
+        href="https://twitter.com/knyttneve"
+        class="text-gray-600 ml-1"
+        rel="noopener noreferrer"
+        target="_blank">@knyttneve</a
+      >
     </p>
-    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+    <span
+      class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
+    >
+      <!-- svelte-ignore a11y-missing-attribute -->
       <a class="text-gray-500">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        <svg
+          fill="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+          />
         </svg>
       </a>
+      <!-- svelte-ignore a11y-missing-attribute -->
       <a class="ml-3 text-gray-500">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        <svg
+          fill="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+          />
         </svg>
       </a>
+      <!-- svelte-ignore a11y-missing-attribute -->
       <a class="ml-3 text-gray-500">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
         </svg>
       </a>
+      <!-- svelte-ignore a11y-missing-attribute -->
       <a class="ml-3 text-gray-500">
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        <svg
+          fill="currentColor"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="0"
+          class="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="none"
+            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+          />
+          <circle cx="4" cy="4" r="2" stroke="none" />
         </svg>
       </a>
     </span>
   </div>
 </footer>
 
-
-
 <style>
-    .subnav {
-        display: flex;
-        height: 20px !important;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 1rem;
-    }
+  .subnav {
+    display: flex;
+    height: 20px !important;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
+  }
 
-    #firstnav{
-      height: 40px;
-      min-height: 40px;
-    }
-    #secnav{
-      height: 40px;
-      min-height: 40px;
-    }
+  #firstnav {
+    height: 45px;
+    min-height: 45px;
+  }
+  #secnav {
+    height: 40px;
+    min-height: 40px;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,40 @@
 <script>
-  import { slide,fade } from 'svelte/transition';
-  let images=[
-    "https://img.freepik.com/free-photo/young-beautiful-stylish-woman-wedding-dress_285396-7822.jpg?w=826&t=st=1677540031~exp=1677540631~hmac=6a2361f0648494b80a130461b397d2df05561817ff6d745a6c0b65904be69149" ,
+  import { slide, fade } from "svelte/transition";
+  let images = [
+    "https://img.freepik.com/free-photo/young-beautiful-stylish-woman-wedding-dress_285396-7822.jpg?w=826&t=st=1677540031~exp=1677540631~hmac=6a2361f0648494b80a130461b397d2df05561817ff6d745a6c0b65904be69149",
     "https://img.freepik.com/free-photo/beautiful-model-girl-white-dress-man-suit_1328-1779.jpg?w=826&t=st=1677540225~exp=1677540825~hmac=918ca7b63749fd56b72a275a568892aaa0da7a611e612f2edd9a34793c5e51b4",
     "https://img.freepik.com/premium-photo/beautiful-young-brunette-woman-choosing-wedding-dress-bridal-salon_633611-4120.jpg?w=826",
-    "https://img.freepik.com/free-photo/elegant-bride-puts-wedding-gown-her-room_146671-19765.jpg?w=826&t=st=1677540176~exp=1677540776~hmac=60262550c6d59c7039d271ca6c53ca04bbe76f2556f1bfb522d6e9799901b5e1" ,
-    "https://img.freepik.com/free-photo/young-woman-celebrating-her-quinceanera_23-2149449182.jpg?w=826&t=st=1677541511~exp=1677542111~hmac=b9cf8f633aaaf46c3a0d76d5a0804fe1cc1200bfa98f2a2746ba21b61179e6b1" ,
+    "https://img.freepik.com/free-photo/elegant-bride-puts-wedding-gown-her-room_146671-19765.jpg?w=826&t=st=1677540176~exp=1677540776~hmac=60262550c6d59c7039d271ca6c53ca04bbe76f2556f1bfb522d6e9799901b5e1",
+    "https://img.freepik.com/free-photo/young-woman-celebrating-her-quinceanera_23-2149449182.jpg?w=826&t=st=1677541511~exp=1677542111~hmac=b9cf8f633aaaf46c3a0d76d5a0804fe1cc1200bfa98f2a2746ba21b61179e6b1",
     "https://img.freepik.com/free-photo/young-beautiful-stylish-woman-wedding-dress_285396-7876.jpg?w=826&t=st=1677540075~exp=1677540675~hmac=61bfe705f701e43138aae1e0a00eb3db24ba76da2d693de909b350bbda60beff",
-    "https://img.freepik.com/free-photo/beautiful-bride-portrait-beach_23-2149044012.jpg?w=826&t=st=1677541563~exp=1677542163~hmac=2175416175fe5c25b48f4fa669af29d84be8dcdf22257c031f4deb99b9f1b6fc" ,
-  ]
-  let i=0;
-  
-  let interval = setInterval(()=>{
+    "https://img.freepik.com/free-photo/beautiful-bride-portrait-beach_23-2149044012.jpg?w=826&t=st=1677541563~exp=1677542163~hmac=2175416175fe5c25b48f4fa669af29d84be8dcdf22257c031f4deb99b9f1b6fc",
+  ];
+  let i = 0;
+
+  let interval = setInterval(() => {
     i++;
-    if(i==images.length){
-      i=0;
+    if (i == images.length) {
+      i = 0;
     }
-  },4000);
+  }, 4000);
 </script>
 
 <svelte:head>
-    <title>Dream Dress</title>
+  <title>Dream Dress</title>
 </svelte:head>
 
-
-<div class="carousel">
-    <div class="carousel-item w-full" transition:fade>
-      <img src={images[i]} alt="Burger" class="w-full h-[500px]" transition:fade />
-    </div> 
-    <!-- <div class="carousel-item w-full">
+<div
+  class="carousel mx-2 md:mx-0  rounded-xl md:rounded-none md:h-full mt-4 md:mt-0 h-64"
+>
+  <div class="carousel-item w-full" transition:fade>
+    <img
+      src={images[i]}
+      alt="Burger"
+      class="md:w-full md:h-[600px] md:object-cover md:origin-center"
+      transition:fade
+    />
+  </div>
+  <!-- <div class="carousel-item w-full">
       <img src="https://img.freepik.com/free-photo/beautiful-model-girl-white-dress-man-suit_1328-1779.jpg?w=826&t=st=1677540225~exp=1677540825~hmac=918ca7b63749fd56b72a275a568892aaa0da7a611e612f2edd9a34793c5e51b4" alt="Burger"class="w-full h-[500px]" />
     </div> 
     <div class="carousel-item w-full">
@@ -45,71 +51,130 @@
     </div> 
     <div class="carousel-item w-full">
       <img src="/https://img.freepik.com/free-photo/beautiful-bride-portrait-beach_23-2149044012.jpg?w=826&t=st=1677541563~exp=1677542163~hmac=2175416175fe5c25b48f4fa669af29d84be8dcdf22257c031f4deb99b9f1b6fc" alt="Burger" class="w-full h-[500px]"/> -->
-    <!-- </div> -->
-  </div>
+  <!-- </div> -->
+</div>
 
-  <div class="bg-white py-6 sm:py-8 lg:py-12">
-    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-8 md:mb-12">Collections</h2>
-  
-      <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
-        <!-- product - start -->
-        <div>
-          <a href="/saree" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
-            <img src="https://i.ytimg.com/vi/Ed0w7l3yqLM/maxresdefault.jpg" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-  
-            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
-              <span class="text-gray-500">Men</span>
-              <span class="text-gray-800 text-lg lg:text-xl font-bold">Saree</span>
-            </div>
-          </a>
-        </div>
-        <!-- product - end -->
-  
-        <!-- product - start -->
-        <div>
-          <a href="/Lahenga" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
-            <img src="https://i.etsystatic.com/29654904/r/il/38f282/3197828949/il_fullxfull.3197828949_l7pe.jpg" loading="lazy" alt="Photo by engin akyurt" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-  
-            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
-              <!-- <span class="text-gray-500">Women</span> -->
-              <span class="text-gray-800 text-lg lg:text-xl font-bold">Lahenga</span>
-            </div>
-          </a>
-        </div>
-        <!-- product - end -->
-  
-        <!-- product - start -->
-        <div>
-          <a href="/Gowns" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
-            <img src="https://i0.wp.com/lovejunebridal.com/wp-content/uploads/2022/04/1-Copy.jpg?resize=600%2C479&ssl=1" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-  
-            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
-              <!-- <span class="text-gray-500">Men</span> -->
-              <span class="text-gray-800 text-lg lg:text-xl font-bold">Gowns</span>
-            </div>
-          </a>
-        </div>
-        <!-- product - end -->
-  
-        <!-- product - start -->
-        <div>
-          <a href="/Salwar" class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
-            <img src="https://i.pinimg.com/originals/55/82/15/558215a6084dfa8f6bc461f6d178fa64.jpg" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-  
-            <div class="w-full flex flex-col bg-white text-center rounded-lg relative p-4">
-              <!-- <span class="text-gray-500">Women</span> -->
-              <span class="text-gray-800 text-lg lg:text-xl font-bold">Salwar</span>
-            </div>
-          </a>
-        </div>
-        <!-- product - end -->
+<div class="bg-slate-100 py-6 sm:py-8 lg:py-12">
+  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+    <h2
+      class="text-gray-800 text-2xl lg:text-5xl font-bold text-center mb-8 md:mb-12"
+    >
+      Collections
+    </h2>
+
+    <div
+      class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6"
+    >
+      <!-- product - start -->
+      <div>
+        <a
+          href="/saree"
+          class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4 hover:shadow-2xl "
+        >
+          <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img
+            src="https://i.ytimg.com/vi/Ed0w7l3yqLM/maxresdefault.jpg"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div
+            class="w-full flex flex-col bg-white text-center rounded-lg relative p-4"
+          >
+            <span class="text-blue-500 ">Cultural</span>
+            <span class="text-gray-800 text-lg lg:text-2xl font-bold"
+              >Saree</span
+            >
+          </div>
+        </a>
       </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a
+          href="/Lahenga"
+          class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4"
+        >
+          <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img
+            src="https://i.etsystatic.com/29654904/r/il/38f282/3197828949/il_fullxfull.3197828949_l7pe.jpg"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div
+            class="w-full flex flex-col bg-white text-center rounded-lg relative p-4"
+          >
+            <!-- <span class="text-gray-500">Women</span> -->
+            <span class="text-blue-500 ">Luxurious</span>
+            <span class="text-gray-800 text-lg lg:text-2xl font-bold"
+              >Lahenga</span
+            >
+          </div>
+        </a>
+      </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a
+          href="/Gowns"
+          class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4"
+        >
+          <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img
+            src="https://i0.wp.com/lovejunebridal.com/wp-content/uploads/2022/04/1-Copy.jpg?resize=600%2C479&ssl=1"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div
+            class="w-full flex flex-col bg-white text-center rounded-lg relative p-4"
+          >
+            <span class="text-blue-500">Stylish</span>
+            <span class="text-gray-800 text-lg lg:text-2xl font-bold"
+              >Gowns</span
+            >
+          </div>
+        </a>
+      </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a
+          href="/Salwar"
+          class="group h-96 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4"
+        >
+          <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img
+            src="https://i.pinimg.com/originals/55/82/15/558215a6084dfa8f6bc461f6d178fa64.jpg"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div
+            class="w-full flex flex-col bg-white text-center rounded-lg relative p-4"
+          >
+            <span class="text-blue-500">Ethnic motifs</span>
+            <span class="text-gray-800 text-lg lg:text-2xl font-bold"
+              >Salwar</span
+            >
+          </div>
+        </a>
+      </div>
+      <!-- product - end -->
     </div>
   </div>
+</div>
 
 <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100) !important;
-    }
-  </style>
+  :global(html) {
+    background-color: theme(colors.gray.100) !important;
+  }
+</style>
