1ae05beaf9327924cb785292991d3a0a1861dd00
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-03-04 22:53:48 +0530

    added changes

--- src/routes/saree/+page.svelte
+++ src/routes/saree/+page.svelte
@@ -1,11 +1,10 @@
-<h1>Saree</h1>
 
 <div class="bg-white py-6 sm:py-8 lg:py-12">
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
       <div class="flex justify-between items-end gap-4 mb-6">
-        <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">Selected</h2>
+        <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">Collection of Saree</h2>
   
-        <a href="#" class="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3">Show more</a>
+        <!-- <a href="#" class="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3">Show more</a> -->
       </div>
   
       <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8">
